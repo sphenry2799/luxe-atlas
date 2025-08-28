ca436522bf86e4265005c3389c3a31f8412422ed
sphenry2799 <sp.henry2799@gmail.com>
2025-08-28 15:49:53 +0100

    add nav bar javascript to file

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,5 +63,20 @@ $(document).ready(function () {
       return false;
     }
   }
-  // Google Maps
+  // Nav Bar
+  document.querySelectorAll(".navbar-collapse .nav-link").forEach((link) => {
+    link.addEventListener("click", function (e) {
+      let section = document.querySelector(e.target.getAttribute("href"));
+      if (section) {
+        e.preventDefault(); // Prevent default anchor click behavior
+        let navbarHeight =
+          document.querySelector(".navbar-toggler").offsetHeight;
+        window.scroll({
+          top: section.offsetTop - navbarHeight, // Adjust for navbar height
+          behavior: "smooth",
+        });
+        document.querySelector(".navbar-collapse").classList.remove("show"); // Collapse navbar
+      }
+    });
+  });
 });
