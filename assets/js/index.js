e03ca2fe762bf06c6ef33766356ccb45286cf878
sphenry2799 <sp.henry2799@gmail.com>
2025-08-29 18:54:51 +0100

    Add to google maps js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 47.6059, lng: -122.3345 }, // Coordinates of Seattle
+    center: { lat: 51.5072 lng: 0.1276 }, // Coordinates of Seattle
     zoom: 8,
+    mapTypeId: "roadmap",
   });
 });
