39a383437818d13d8f6f864f5c44d3bcb75aa389
sphenry2799 <sp.henry2799@gmail.com>
2025-09-06 19:12:05 +0100

    Add script for scroll.js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -31,3 +31,28 @@ $(document).ready(function () {
 
   showCity();
 });
+$(document).ready(function () {
+  function greeting() {
+    let $items = $(".greeting");
+    let hour = new Date().getHours();
+    let message = "";
+
+    if (hour < 6) {
+      message = "Good night";
+    } else if (hour < 12) {
+      message = "Good morning";
+    } else if (hour < 18) {
+      message = "Good afternoon";
+    } else {
+      message = "Good evening";
+    }
+
+    // Insert text into paragraph
+    $items.text(message);
+
+    // Debugging log
+    console.log("Hour:", hour, "→ Message:", message);
+  }
+
+  greeting(); // run once
+});
