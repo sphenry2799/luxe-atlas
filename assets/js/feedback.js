cba47fe39c5680d359b4a865ce0b36fdd300f53c
sphenry2799 <sp.henry2799@gmail.com>
2025-09-09 18:43:51 +0100

    Rename file to feedback.js

---
+++ assets/js/feedback.js
@@ -0,0 +1,26 @@
+(function () {
+  emailjs.init("M7YirBCJ61f49tPKe"); // replace with your EmailJS public key
+})();
+
+const btn = document.getElementById("button");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "contact_service"; // replace with your actual service ID
+  const templateID = "template_3eqgm7w"; // replace with your template ID
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
