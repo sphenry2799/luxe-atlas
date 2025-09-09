f1052f0cbf2893af98cd2245dc5cb6892a985508
sphenry2799 <sp.henry2799@gmail.com>
2025-09-09 18:44:16 +0100

    Add contact form javascript to contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,15 +3,14 @@
 })();
 
 const btn = document.getElementById("button");
-const form = document.getElementById("form");
 
-form.addEventListener("submit", function (event) {
+document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
   btn.value = "Sending...";
 
-  const serviceID = "contact_service"; // replace with your actual service ID
-  const templateID = "template_3eqgm7w"; // replace with your template ID
+  const serviceID = "contact_service";
+  const templateID = "template_14n6mnn";
 
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
