d12a403be08fdf06703f2ba4e4b6f62467b7133d
sphenry2799 <sp.henry2799@gmail.com>
2025-09-12 16:29:41 +0100

    Add comments to contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,11 @@
+// Function to link Email JS to javascript
 (function () {
   emailjs.init("M7YirBCJ61f49tPKe"); // replace with your EmailJS public key
 })();
 
 const btn = document.getElementById("button");
 
+// Clicking the submit button to send email
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
